aede864f04422024980891024f1f7759beca7999
gravo-lan <alvintsolh@gmail.com>
2023-12-04 10:06:34 +0800

    Completed carmichael totient function, modular multiplicative inverse generation and e value generation

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) {
         System.out.println("Enter a message");
         Scanner scanner = new Scanner(System.in);
         String message = scanner.nextLine();
         char[] letters = message.toCharArray();
 
-        int[] pq = genPQ();
+        //int[] pq = genPQ();
+        int[] pq = new int[]{61,53};
         int c = carmichael(pq[0],pq[1]);
+        System.out.println(c);
         BigInteger n = BigInteger.valueOf((long) pq[0] * pq[1]);
         int e = genE(c,pq[0],pq[1]);
-        int d = genD(e,c);
+        BigInteger d = genD(BigInteger.valueOf(e),BigInteger.valueOf(c));
+        System.out.println((BigInteger.valueOf(e).multiply(d)).mod(BigInteger.valueOf(c)));
 
         StringBuilder result = new StringBuilder();
         for (char l : letters) {
@@ -25,7 +27,7 @@ public class Main {
         System.out.println(crack(result.toString(),d,n));
     }
 
-    public static String crack(String m, int d, BigInteger n) throws UnsupportedEncodingException {
+    public static String crack(String m, BigInteger d, BigInteger n) {
         String[] chars = m.split("\\.");
         StringBuilder result = new StringBuilder();
         for (String s: chars) {
@@ -34,14 +36,14 @@ public class Main {
         return result.toString();
     }
 
-    public static String decrypt(BigInteger l, int d, BigInteger n) throws UnsupportedEncodingException {
-        BigInteger val = ((l.pow(d)).mod(n));
+    public static String decrypt(BigInteger l, BigInteger d, BigInteger n) {
+        BigInteger val = l.modPow(d,n);
         return new String(val.toByteArray(), StandardCharsets.UTF_8);
     }
 
     public static BigInteger encrypt(char l, int e, BigInteger n) {
         BigInteger x = new BigInteger(String.valueOf((int)l));
-        return (x.pow(e)).mod(n);
+        return x.modPow(BigInteger.valueOf(e),n);
     }
 
     public static int[] genPQ() {
@@ -65,11 +67,12 @@ public class Main {
         return e;
     }
 
-    public static int genD (int e, int c) {
-        for (int i = 0; i < c; i++) {
-            if ((e*i) % c == 1) return i;
+    public static BigInteger genD (BigInteger e, BigInteger c) {
+        for (BigInteger x = BigInteger.valueOf(0); x.compareTo(c) < 0; x=x.add(BigInteger.ONE)) {
+            if ((e.multiply(x)).mod(c).equals(BigInteger.valueOf(1))) return x;
         }
-        return 1;
+        System.out.println("No valid d found");
+        return BigInteger.valueOf(1);
     }
 
     public static int gcd(int a, int b) {
@@ -79,10 +82,10 @@ public class Main {
 
     private static boolean isPrime(int inputNum){
         if (inputNum <= 3 || inputNum % 2 == 0)
-            return inputNum != 2 && inputNum != 3; //this returns false if number is <=1 & true if number = 2 or 3
+            return inputNum != 2 && inputNum != 3;
         int divisor = 3;
         while ((divisor <= Math.sqrt(inputNum)) && (inputNum % divisor != 0))
-            divisor += 2; //iterates through all possible divisors
-        return inputNum % divisor == 0; //returns true/false
+            divisor += 2;
+        return inputNum % divisor == 0;
     }
 }
